b5bfacc944dd1516428837fb60e2b2113cd36616
Eric Eggert <mail@yatil.net>
2015-11-23 18:16:40 +0100

    Display custom fields

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -19,6 +19,27 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php
+			$post_object = get_field('vendor');
+
+			if( $post_object ):
+
+				// override $post
+				$post = $post_object;
+				setup_postdata( $post );
+
+				?>
+			    <div style="border: 3px solid #bbb; padding: 10px;">
+			    	<h3>Vendor: <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			    	<?php //<span>Post Object Custom Field: <?php the_field('field_name'); </span> ?>
+			    </div>
+			    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+			<?php endif; ?>
+		<?php the_field( "description" ); ?>
+		<?php the_field( "website" ); ?>
+		<?php the_field( "a11y_statement" ); ?>
+		<?php the_field( "a11y_statement_url" ); ?>
+		<?php the_field( "development_url" ); ?>
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
