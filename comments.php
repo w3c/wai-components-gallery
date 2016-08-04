6fbc6f2dd6edd6ad6587b2d5ee0f0591b87fc23e
Eric Eggert <mail@yatil.net>
2016-08-04 13:11:33 +0200

    Report issues comment form

--- comments.php
+++ comments.php
@@ -77,6 +77,45 @@ if ( post_password_required() ) {
 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'wai_components' ); ?></p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+
+
+	<?php
+	$comment_args = array(
+  'id_form'           => 'commentform',
+  'class_form'      => 'comment-form',
+  'id_submit'         => 'submit',
+  'class_submit'      => 'submit',
+  'name_submit'       => 'submit',
+  'title_reply'       => __( 'Report a problem with this entry' ),
+  'title_reply_to'    => __( 'Leave a Reply to %s' ),
+  'cancel_reply_link' => __( 'Cancel Report' ),
+  'label_submit'      => __( 'Save Report' ),
+  'format'            => 'xhtml',
+
+  'comment_field' =>  '<p class="comment-form-comment"><label for="comment">' . _x( 'Please describe the issue with this component:', 'noun' ) .
+    '</label><textarea id="comment" name="comment" cols="45" rows="3" aria-required="true">' .
+    '</textarea></p>',
+
+  'must_log_in' => '<p class="must-log-in">Messages entered into this form are only for internal use.' .
+    sprintf(
+      __( 'You must be <a href="%s">logged in</a> to post a comment.' ),
+      wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+    ) . '</p>',
+
+  'logged_in_as' => '<p class="logged-in-as">Messages entered into this form are only for internal use. ' .
+    sprintf(
+    __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ),
+      admin_url( 'profile.php' ),
+      $user_identity,
+      wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+    ) . '</p>',
+
+  'comment_notes_before' => '<p class="comment-notes">' .
+    __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) .
+    '</p>',
+
+  'comment_notes_after' => '',
+);
+	comment_form($comment_args); ?>
 
 </div><!-- #comments -->
