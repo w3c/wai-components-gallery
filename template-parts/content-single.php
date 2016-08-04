5c869a1a955ffc4e72bf9acab848402f887387ba
Eric Eggert <mail@yatil.net>
2016-08-04 13:20:30 +0200

    content-single & content-tiles
    For detail pages and tiles on the front page

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -7,36 +7,124 @@
  * @package WAI_Components_Gallery
  */
 
+if (!function_exists('curlurl')) {
+  function curlurl($url) {
+    $ch = curl_init();
+
+    // set URL and other appropriate options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'W3C-WAI');
+
+    // grab URL and pass it to the browser
+    $data = curl_exec($ch);
+    // echo('<!--'."\n");
+    // var_dump($data);
+    // echo("\n".'-->');
+    curl_close($ch);
+    return json_decode ( $data );
+  }
+}
+
 ?>
 
+<div class="search-filter-results">
+<div class="results-details">
+
 <?php $field = get_fields(); ?>
-<div class="toolbar">
-	<button>
-		<?php echo wai_icon( "share" ); ?>
-		Share link to this widget
-	</button>
-	<?php get_search_form(true); ?>
-</div>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<?php $comptype = array(); ?>
+<?php if($field['component_type']) { ?>
+    <?php $comptype[] = $field['component_type']->name; ?>
+<?php } ?>
+
+<article id="component-<?php the_ID();?>" <?php post_class('tile');?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title( ); ?> <small>by <?php echo $field['vendor']->name; ?></small></h1>
+		<h1 class="entry-title"><a href="<?php the_field( "website" ); ?>"><?php the_title(); ?> <?php echo wai_icon('link-external', 'External Link'); ?></a> <small><span class="byline">by <?php echo $field['vendor']->name; ?></span></small></h1>
+
+    <?php if ( 'post' === get_post_type() ) : ?>
+    <div class="entry-meta">
+      <?php wai_components_posted_on(); ?>
+    </div><!-- .entry-meta -->
+    <?php endif; ?>
+    <div class="permalink_wrapper"><a href="#component-<?php the_ID();?>" class="btn"><?php echo wai_icon('share'); ?><use xlink:href="#i-share"></use></svg>&nbsp;Share</a><div class="sharebox"><p><label>Link to this view:<input value="" readonly="" type="url"> Shortcut to copy the link: <kbd>ctrl</kbd>+<kbd>C</kbd> <em>or</em> <kbd>⌘</kbd><kbd>C</kbd></label></p><p><a href="">E-mail a link to this section</a><button>Close</button></p></div></div>
 	</header><!-- .entry-header -->
-	<div class="entry-content entry-content-wrapper">
-		<div class="entry-content-main">
-			<?php the_field( "description" ); ?>
-			<div class="links">
-				<a href="<?php the_field( "website" ); ?>"><?php echo wai_icon( "home" ); ?> <?php _e('Website', 'wai_components'); ?></a>
-				<?php if( get_field( 'a11y_statement' ) ): ?>
-				<a href="<?php the_field( "a11y_statement_url" ); ?>"><?php echo wai_icon( "accessibility" ); ?> <?php _e('Accessibility Statement', 'wai_components'); ?></a>
-				<?php endif; ?>
-				<!-- TODO: If Github: Show github logo. If not, show generic cloud symbol. -->
-				<a href="<?php the_field( "development_url" ); ?>"><?php echo wai_icon( "cloud" ); ?> <?php _e('Development URL', 'wai_components'); ?></a>
-			</div>
-			<div class="entry-meta">
-				<?php wai_components_posted_on(); ?>
-			</div><!-- .entry-meta -->
-				<button onclick="document.querySelector('#comment_form').removeAttribute('hidden');"><?php echo wai_icon( 'warning' ); ?> Report a problem with this entry</button>
-				<div hidden id="comment_form">
+	<div class="entry-content">
+		<div class="thumbnail">
+      <a aria-hidden="true" href="<?php the_field( "website" ); ?>">
+        <img src="<?php bloginfo('template_url'); ?>/screenshot.php?url=<?php urlencode(the_field( "website" )); ?>&amp;w=200" alt="Screenshot of “<?php the_title( ); ?>”">
+      </a>
+    </div>
+    <div class="description">
+      <div class="desc-inner">
+        <p>
+        <?php
+          if($comptype) {
+            echo ('<span class="visuallyhidden">Component Type:</span>');
+            foreach ($comptype as $type) {
+              echo ('<span class="type type-'.strtolower($type).'">'.$type.'');
+              if('Theme' == $type) {
+                echo ' <span>for '.get_field( 'cms' )->name.'</span>';
+              }
+              echo ('</span> ');
+            }
+          }
+          if ( strlen(strip_tags($field['description'])) > 600 ) {
+            echo substr(strip_tags($field['description']),0,500).'…';
+          } else {
+            echo strip_tags($field['description']);
+          }
+        ?>
+        </p>
+        <p><a href="<?php the_field( "website" ); ?>"><?php the_field( "website" ); ?></a></p>
+      </div>
+    <?php
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wai_components' ),
+        'after'  => '</div>',
+      ) );
+    ?>
+    <aside class="moreinfo">
+      <ul>
+      <?php if($field['tags']) { ?>
+        <li class="tags">
+          <h3>Tags</h3>
+          <ul>
+            <?php foreach ($field['tags'] as $tag) { ?>
+              <li><?php /*<a href="<?php echo get_term_link($tag) ?>">*/?><?php echo $tag->name ?><?php /*</a>*/?></li>
+            <?php } ?>
+          </ul>
+        </li>
+      <?php } ?>
+      <?php if(get_field('license')) { ?>
+        <li><strong>License:</strong> <?php echo get_field('license')->name; ?></li>
+      <?php } ?>
+      <?php if(get_field('a11y_statement_url')) { ?>
+        <li><strong>Accessibility:</strong> <a href="<?php echo get_field('a11y_statement_url'); ?>">Statement provided</a></li>
+      <?php } ?>
+      <?php if (get_field("development_url")) { ?>
+        <li><strong>Repository:</strong> <a href="<?php the_field("development_url"); ?>">Source Code Available</a>
+        <?php if ( strpos ( get_field( "development_url" ) , 'github.com' ) !== FALSE ) {
+          $gh = preg_split ( '/\//' , str_replace(array("http://", "https://", 'www.', 'github.com/'), "", get_field( "development_url" ) ) );
+
+          $gh['user'] = $gh[0];
+          $gh['repo'] = $gh[1];
+
+          $jdata = curlurl('https://api.github.com/repos/'.$gh['user'].'/'.$gh['repo']);
+
+          // create a new cURL resource
+
+          ?>
+          <span class="github">(Github Statistics: <?php echo wai_icon( "gh-star", "" ); ?> <?php echo $jdata->subscribers_count ?> Stars, <?php echo wai_icon( "gh-forked", "" ); ?> <?php echo $jdata->forks ?> Forks, Last Update: <?php echo strftime('%F', strtotime($jdata->updated_at)) ?>)</span>
+          <?php } ?>
+          </li>
+          <?php } ?>
+          <li><strong>Component submitted:</strong> <?php the_modified_date("Y-m-d"); ?></li>
+        </ul>
+        <div class="entry-report" id="report">
+			<button onclick="if (document.querySelector('#comment_form').getAttribute('hidden')) {document.querySelector('#comment_form').removeAttribute('hidden');} else {document.querySelector('#comment_form').setAttribute('hidden', true);}"><?php echo wai_icon( 'warning' ); ?> Report a problem with this entry</button>
+				<div id="comment_form">
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
@@ -44,46 +132,16 @@
 					endif;
 				?>
 				</div>
+				<script>
+					if ('#report' !== window.location.hash) {
+						document.querySelector('#comment_form').setAttribute('hidden', true);
+					}
+				</script>
+				</div>
+      </aside>
 		</div>
-		<div class="entry-content-side">
-			<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-				the_post_thumbnail( 'medium' );
-			} ?>
-			<dl>
-				<dt>License</dt>
-				<dd><a href="<?php echo get_term_link( get_field( "license" ) ); ?>"><?php echo get_field( "license" )->name; ?></a></dd>
-		    <dt>Vendor</dt>
-				<dd><a href="<?php echo get_term_link( get_field( "vendor" ) ); ?>"><?php echo get_field( "vendor" )->name; ?></a></dd>
-				<?php if ( get_field( "contact_url" ) ) { ?>
-					<dt>Contact URL</dt>
-					<dd><a href="<?php echo get_field( "contact_url" ); ?>"><?php echo get_field( "contact_url" ); ?></a></dd>
-				<?php } elseif ( get_field( "contact_email" ) ) { ?>
-					<dt>Contact Email</dt>
-					<dd><a href="mailto:<?php echo get_field( "contact_email" ); ?>"><?php echo get_field( "contact_email" ); ?></a></dd>
-				<?php } ?>
-			</dl>
-			<?php // if ( strpos ( get_field( "development_url" ) , 'github.com' ) !== FALSE ) { ?>
-			<p><strong>Github statistics for this project:</strong></p>
-			<ul>
-				<li>Latest commit to master: 2015-12-12</li>
-		    <li>Readme information: <a href="#">Readme.txt</a></li>
-				<li>Stars: 3</li>
-				<li>Forks: 27</li>
-				<li>Contributors: Max Mustermann</li>
-			</ul>
-			<?php // } ?>
-		</div>
-		<?php // the_content(); ?>
-		<?php
-			// wp_link_pages( array(
-			// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wai_components' ),
-			// 	'after'  => '</div>',
-			// ) );
-		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php wai_components_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
-
+</div>
+</div>
--- template-parts/content-tiles.php
+++ template-parts/content-tiles.php
@@ -7,53 +7,119 @@
  * @package WAI_Components_Gallery
  */
 
+if (!function_exists('curlurl')) {
+	function curlurl($url) {
+		$ch = curl_init();
+
+		// set URL and other appropriate options
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'W3C-WAI');
+
+		// grab URL and pass it to the browser
+		$data = curl_exec($ch);
+		// echo('<!--'."\n");
+		// var_dump($data);
+		// echo("\n".'-->');
+		curl_close($ch);
+		return json_decode ( $data );
+	}
+}
+
 ?>
 
 <?php $field = get_fields(); ?>
+
+<?php if($field['component_type']) { ?>
+	<?php foreach ($field['component_type'] as $type) { ?>
+		<?php $comptype[] = $type->name; ?>
+	<?php } ?>
+<?php } ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class('tile'); ?>>
-	<a href="<?php echo get_permalink(); ?>">
 	<header class="entry-header">
-		<h2 class="entry-title"><?php the_title(); ?> <small>by <?php echo $field['vendor']->name; ?></small></h2>
+		<h2 class="entry-title"><a href="<?php the_field( "website" ); ?>"><?php the_title(); ?></a> <small><?php if(in_array('Theme', $comptype)) { ?>for <?php echo get_field( 'cms' )->name;?>, <?php } ?><span class="byline">by <?php echo $field['vendor']->name; ?></span></small></h2>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php wai_components_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
+		<?php if($field['component_type']) { ?>
+			<span class="visuallyhidden">Type:</span>
+			<?php foreach ($comptype as $type) { ?>
+				<span class="type"><?php echo $type; ?></span>
+			<?php } ?>
+		<?php } ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<div class="thumbnail">
-		<?php
-			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-				the_post_thumbnail( 'small', array('alt' => '', 'title' => ''));
-			} else {
-				echo wai_icon( "image" );
-			}
-		?>
+			<img src="<?php bloginfo('template_url'); ?>/screenshot.php?url=<?php urlencode(the_field( "website" )); ?>&amp;w=200" alt="Screenshot of “<?php the_title( ); ?>”">
 		</div>
-		<?php
-			/*the_content( sprintf(
-				/* translators: %s: Name of current post. *//*
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wai_components' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );*/
-			if ( strlen($field['description']) > 200 ) {
-			   echo substr($field['description'],0,100).'…';
-			} else {
-				echo $field['description'];
-			}
-		?>
+		<div class="description">
+			<p>
+			<?php
+				/*the_content( sprintf(
+					/* translators: %s: Name of current post. *//*
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wai_components' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );*/
+				if ( strlen(strip_tags($field['description'])) > 600 ) {
+				   echo substr(strip_tags($field['description']),0,500).'…';
+				} else {
+					echo strip_tags($field['description']);
+				}
+			?>
+			</p>
+			<p><a href="<?php the_field( "website" ); ?>"><?php the_field( "website" ); ?></a></p>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wai_components' ),
 				'after'  => '</div>',
 			) );
 		?>
+		<?php if($field['tags']) { ?>
+			<aside class="tags">
+				<h3>Tags</h3>
+				<ul>
+					<?php foreach ($field['tags'] as $tag) { ?>
+						<li><a href="<?php echo get_term_link($tag) ?>"><?php echo $tag->name ?></a></li>
+					<?php } ?>
+				</ul>
+			</aside>
+		<?php } ?>
+			<aside class="moreinfo">
+				<p><?php if(get_field('license')) {?><strong>License:</strong> <?php echo get_field('license')->name; ?> • <?php } ?><strong>Information last updated:</strong> <?php the_modified_date("Y-m-d"); ?><?php if(get_field('a11y_statement')) {?> • <a href="<?php echo get_field('a11y_statement_url'); ?>">Accessibility Statement</a><?php } ?></p>
+			</aside>
+		</div>
 	</div><!-- .entry-content -->
+		<?php
+			if ( strpos ( get_field( "development_url" ) , 'github.com' ) !== FALSE ) {
+				$gh = preg_split ( '/\//' , str_replace(array("http://", "https://", 'www.', 'github.com/'), "", get_field( "development_url" ) ) );
+
+				$gh['user'] = $gh[0];
+				$gh['repo'] = $gh[1];
+
+				$jdata = curlurl('https://api.github.com/repos/'.$gh['user'].'/'.$gh['repo']);
+
+				// create a new cURL resource
+
+		?>
+		<div class="github">
+			<ul>
+				<li>Development <a href="<?php the_field( "development_url" ); ?>">on GitHub</a> <span class="arrow"></span></li>
+				<li><?php echo wai_icon( "gh-star", "Starred:" ); ?> <?php echo $jdata->subscribers_count ?></li>
+				<li><?php echo wai_icon( "gh-forked", "Forked" ); ?> <?php echo $jdata->forks ?></li>
+				<li>Created: <?php echo strftime('%F %R', strtotime($jdata->created_at)) ?></li>
+				<li>Updated: <?php echo strftime('%F %R', strtotime($jdata->updated_at)) ?></li>
+				<li>Owner: <a rel="nofollow" href="<?php echo $jdata->owner->url?>"><img src="<?php echo $jdata->owner->avatar_url?>" class="github-avatar" alt="" width="20" height="20"> <?php echo $jdata->owner->login ?></a></li>
+			</ul>
+		</div>
+		<?php } ?>
 
 	<footer class="entry-footer">
 		<?php wai_components_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
-	</a>
 </article><!-- #post-## -->
